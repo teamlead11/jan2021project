7147d475f74369240dd7503b600978bd14efa658
karthi1306 <skarthick0113@gmail.com>
2021-01-20 13:04:03 +0530

    new code in test case2

--- src/main/java/testpack/TestCase2.java
+++ src/main/java/testpack/TestCase2.java
@@ -5,8 +5,13 @@ public class TestCase2 {
 		// TODO Auto-generated method stub
 
 	}
-public static void main(String[] args) {
-	TestCase2 toj = new TestCase2();
-	toj.data();
-}
+
+	public static void main(String[] args) {
+		TestCase2 toj = new TestCase2();
+		toj.data();
+	}
+
+	public void add(int a, int b) {
+		System.out.println(a + b);
+	}
 }
