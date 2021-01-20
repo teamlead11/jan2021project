907eb81eabb7c5cd582555f5ef1799f8de223c71
karthi1306 <skarthick0113@gmail.com>
2021-01-20 11:17:55 +0530

    added logic to add three numbers

--- src/main/java/testpack/Main.java
+++ src/main/java/testpack/Main.java
@@ -6,7 +6,10 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("my framework component");
 		System.out.println("Framework component added by Karthick");
-		
+
 	}
 
+	public void add(int a, int b, int c) {
+		System.out.println("a+b+c");
+	}
 }
